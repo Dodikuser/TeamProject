0e217bd18773cef5e61b96063c26a7e89a469a57
Dodikuser <dan9228335@gmail.com>
2025-02-21 20:50:02 +0100

    Initial commit

---
+++ WebAPI/WebAPI/Controllers/DataController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/data")]
+    public class DataController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult ReceiveData([FromBody] MessageModel request)
+        {
+            Console.WriteLine($"Received message: {request.Message}");
+            return Ok(new { response = $"Received: {request.Message}" });
+        }
+    }
+
+    public class MessageModel
+    {
+        public string Message { get; set; }
+    }
+
+}
