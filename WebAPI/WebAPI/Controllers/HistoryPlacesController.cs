450c350e017f3b85df00301f6da44f8bb5264a55
Dodikuser <dan9228335@gmail.com>
2025-05-11 19:17:43 +0200

    Створення базових контролерів

---
+++ WebAPI/WebAPI/Controllers/HistoryPlacesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/history/places")]
+    public class HistoryPlacesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll() => Ok();
+
+        [HttpPost("search")]
+        public IActionResult Search([FromBody] object filters) => Ok();
+
+        [HttpPost("add")]
+        public IActionResult Add([FromBody] object place) => Ok();
+
+        [HttpDelete("delete/{id}")]
+        public IActionResult Delete(int id) => Ok();
+
+        [HttpDelete("clear")]
+        public IActionResult Clear() => Ok();
+    }
+
+}
---
+++ WebAPI/WebAPI/Controllers/HistoryRequestsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/history/requests")]
+    public class HistoryRequestsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll() => Ok();
+
+        [HttpPost("search")]
+        public IActionResult Search([FromBody] object filters) => Ok();
+
+        [HttpPost("add")]
+        public IActionResult Add([FromBody] object request) => Ok();
+
+        [HttpDelete("delete/{id}")]
+        public IActionResult Delete(int id) => Ok();
+
+        [HttpDelete("clear")]
+        public IActionResult Clear() => Ok();
+    }
+
+}
