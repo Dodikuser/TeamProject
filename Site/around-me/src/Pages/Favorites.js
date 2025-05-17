f6fb28d03acbaad23bb436f26432fde2ebb9d075
Timbuhtuk <timpfoy@gmail.com>
2025-05-17 17:08:55 +0300

    Main branch cleanup

--- Site/around-me/src/Pages/Favorites.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-export default function Favorites() {
-  return <div className="p-4">⭐ Улюблені місця</div>;
-}
--- android/Maps3/app/src/main/java/com/example/maps1/AuthResponse.java
+++
@@ -1,7 +0,0 @@
-package com.example.maps1;  // Добавлено
-
-public class AuthResponse {
-    public boolean success;
-    public String message;
-    public int userId;
-}
